7b36ec175d5bcfd4db34a02d8001cbb1a4bf7f7d
rijvimahmudd <93908058+rijvimahmudd@users.noreply.github.com>
2024-01-28 11:48:14 +0600

    featured section complete

--- src/components/Featured/index.jsx
+++ src/components/Featured/index.jsx
@@ -1,14 +1,18 @@
-import { useState } from "react"
-import { images } from "./data"
+import { useState } from "react";
+import { images } from "./data";
+
 const Featured = () => {
-    const [imgs, setImgs] = useState(images);
+  const [imgs, setImgs] = useState(images);
+
   return (
     <div>
-        {imgs.map(img=>(
-            <img src={img} key={img}/>
+      <section className="px-20 flex flex-row items-center justify-between py-10 bg-primary">
+        {imgs.map((img) => (
+          <img src={img} key={img} className="w-full h-auto max-w-[130px]" />
         ))}
+      </section>
     </div>
-  )
-}
+  );
+};
 
-export default Featured
+export default Featured;
